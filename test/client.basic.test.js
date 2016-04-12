d03db3b2f5dee15ec1d84bb68d48bf6bc919149a
dfberry <dinaberry@outlook.com>
2016-04-12 13:25:30 -0700

    karma works but tests fail

---
+++ test/client.basic.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+describe('User menu', function() {
+  var injector;
+  var element;
+  var scope;
+  var intercepts;
+  var httpBackend;
+  
+  var configData = {"name": "myApp"};
+
+  beforeEach(function() {
+    injector = angular.injector(['myApp', 'ngMockE2E']);
+    intercepts = {};
+
+    injector.invoke(function($rootScope, $compile, $httpBackend) {
+      scope = $rootScope.$new();
+
+      $httpBackend.whenGET(/.*\/templates\/.*/i).passThrough();
+      httpBackend = $httpBackend;
+
+      element = $compile('<user-menu></user-menu>')(scope);
+      scope.$apply();
+    });
+  });
+
+  it('shows logged config info', function(done) {
+    httpBackend.expectGET('/config/v1/json').respond(configData);
+
+    scope.$on('UserMenuController', function() {
+      assert.equal(element.find('.config').text().trim(), configData);
+
+      httpBackend.flush();
+      
+      // TODO: test css values
+      //assert.notEqual(element.find('.user-info .user').css('display'), 'none');
+      //assert.equal(element.find('.user-info .user img').attr('src'), 'myPic');
+      done();
+    });
+  });
+});
