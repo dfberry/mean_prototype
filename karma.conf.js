ffe6c420b69dfdf06a6b6a75f15187a0e60d0824
dfberry <dinaberry@outlook.com>
2016-04-18 14:33:30 -0700

    beginning of karma ng tests

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+      'http://code.jquery.com/jquery-1.11.3.js',
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular.js',
+      // For ngMockE2E
+      'https://ajax.googleapis.com/ajax/libs/angularjs/1.4.0/angular-mocks.js',
+      './public/index.js',
+      './test/client.*.test.js',
+      { pattern: './public/templates/*.html', included: false, served: true }
+    ],
+    frameworks: ['mocha', 'chai'],
+    browsers: ['Chrome'],
+    port: 9876,
+    proxies : {
+      '/': 'http://localhost:9876/base/'
+    }
+  });
+};
